fd0d1b85706f69a834fc8dc46e936d8c41dae519
Andrea Petri <ap3020@columbia.edu>
2025-08-11 13:22:22 -0400

    implement bar chart

--- gui/dashboard.py
+++ gui/dashboard.py
@@ -1,3 +1,5 @@
+import typing
+
 from deephaven import ui,agg
 
 from deephaven.table import Table
@@ -5,16 +7,26 @@ from deephaven.pandas import to_pandas
 
 from . import traces
 
+# Globals
 CLAUSE = {
     "int" : lambda c,v:f"{c}={v}",
     "double" : lambda c,v:f"{c}={v}",
     "java.lang.String" : lambda c,v:f"{c}=`{v}`"
 }
 
+CONFIG = {
+    "chart_types" : ["bars"],
+    "filterable" : lambda t: [c for c in t.column_names if not c.startswith("value")],
+    "aggregations":{
+        "average1": agg.avg("average1 = value1"),
+        "average2": agg.avg("average2 = value2"),
+        "average3": agg.avg("average3 = value3")
+    }
 
-def filtering(t:Table,filter_values,set_filter_values):
+}
 
-    ctyp = ui.use_memo(lambda: dict(to_pandas(t.meta_table)[["Name","DataType"]].values),[t])
+# Filtering
+def filteringControls(t:Table,filterable:typing.List[str],filter_values:typing.Dict,set_filter_values:typing.Callable) -> typing.Dict:
 
     # Text box
     txt = ui.text(f"Filters: " + str({k:v for k,v in filter_values.items() if v is not None}))
@@ -28,43 +40,118 @@ def filtering(t:Table,filter_values,set_filter_values):
                      selected_key=filter_values[c] if c in filter_values else None,
                      on_change=lambda v,x=c: set_filter_values({**filter_values,x:v}))
 
-        for c in t.column_names if not c.startswith("value")
+        for c in filterable
     ]
 
     # Clear all filters
     clear = ui.button("Clear all filters",on_press=lambda b: set_filter_values({x:None for x in filter_buttons}))
 
-    # Filter clauses
+    # Done
+    return {
+        "filter_text" : txt,
+        "filter_buttons" : filter_buttons,
+        "clear_button" : clear
+    }
+
+
+def filterTable(t:Table,filter_values:typing.Dict) -> typing.Dict:
+
+    ctyp = ui.use_memo(lambda: dict(to_pandas(t.meta_table)[["Name","DataType"]].values),[t])
+
+    # Do the filtering
     clauses = ui.use_memo(lambda:[CLAUSE[ctyp[x]](x,filter_values[x]) for x in filter_values if filter_values[x] is not None],[filter_values])
     tfilt = ui.use_memo(lambda: t.where(clauses),[t,clauses])
 
     return {
-        "filter_text" : txt,
-        "filter_buttons" : filter_buttons,
-        "clear_button" : clear,
         "filter_clauses" : clauses,
         "filtered_table" : tfilt
     }
 
+# Aggregations
+def aggregationControls(filterable:typing.List[str],by_values:typing.List,set_by_values:typing.Callable,metrics:typing.List[str],metric_value:str,set_metric_value:typing.Callable) -> typing.Dict:
+
+    # Set buttons
+    by_buttons = [
+        ui.picker(*filterable,selected_key=by_values[0],on_change=lambda v:set_by_values([v,by_values[1]]),label="Primary by"),
+        ui.picker(*["NONE"]+filterable,selected_key=by_values[1],on_change=lambda v:set_by_values([by_values[0],v]),label="Secondary by")
+    ]
+
+    metric_button = ui.picker(*metrics,selected_key=metric_value,on_change=set_metric_value,label="Aggregation metric")
+
+    # Done
+    return {
+        "by_buttons" : by_buttons,
+        "metric_button" : metric_button,
+    }
+
+def aggregateTable(tfilt:Table,by_values:typing.List,metric_value:str) -> typing.Dict:
+
+    # Run aggregations
+    byv = [b for b in by_values if b!="NONE"]
+    tagg = ui.use_memo(lambda:tfilt.agg_by([CONFIG["aggregations"][metric_value]],by=byv).sort(byv),[tfilt,by_values,metric_value])
+
+    return {
+        "aggregated_table": tagg
+    }
+
+# Charting
+def chartControls() -> typing.Dict:
+
+    # Graph type button
+    chart_button = ui.picker(*CONFIG["chart_types"],selected_key=CONFIG["chart_types"][0],label="Chart type")
+
+    return {
+        "chart_button": chart_button
+    }
+
+def chartTable(tagg:Table,bys:typing.List[str],metric:str,chart_type:str):
+
+    byv = [b for b in bys if b!="NONE"]
+
+    match chart_type:
+        case "bars":
+            return ui.use_memo(lambda:traces.bars(tagg,byv,metric),[tagg,byv,metric])
+        case _:
+            raise ValueError(f"Chart type:{chart_type} not implemented")
+
+
+##########################################################
+##########################################################
 
 @ui.component
 def arrange(t:Table):
 
+    filterable = CONFIG["filterable"](t)
+    metrics = list(CONFIG["aggregations"].keys())
+
     # State management
     filter_values,set_filter_values = ui.use_state({})
+    by_values,set_by_values = ui.use_state([filterable[0],"NONE"])
+    metric_value,set_metric_value = ui.use_state(metrics[0])
+    chart_type,set_chart_type = ui.use_state(CONFIG["chart_types"][0])
 
     # Filtering
-    filt = filtering(t,filter_values,set_filter_values)
+    filt = {}
+    filt.update(filteringControls(t,filterable,filter_values,set_filter_values))
+    filt.update(filterTable(t,filter_values))
 
-    ui.panel(filt["filter_text"],filt["clear_button"]," AND ".join(filt["filter_clauses"]),ui.flex(*filt["filter_buttons"],wrap="wrap"),title="Filtering controls")
+    # Aggregations
+    aggs = {}
+    aggs.update(aggregationControls(filterable,by_values,set_by_values,metrics,metric_value,set_metric_value))
+    aggs.update(aggregateTable(filt["filtered_table"],by_values,metric_value))
 
-    ui.column(
-            ui.panel(filt["filter_text"],filt["clear_button"]," AND ".join(filt["filter_clauses"]),ui.flex(*filt["filter_buttons"],wrap="wrap"),title="Filtering controls"),
-            ui.panel(filt["filtered_table"],title="Filtered table")
-        )
+    # Charting
+    chrt = chartControls()
+    chrt["chart"] = chartTable(aggs["aggregated_table"],by_values,metric_value,chart_type)
 
     # Arrange
     return ui.column(
-        ui.panel(filt["filter_text"],filt["clear_button"]," AND ".join(filt["filter_clauses"]),ui.flex(*filt["filter_buttons"],wrap="wrap"),title="Filtering controls"),
-        ui.panel(filt["filtered_table"],title="Filtered table")
+        ui.row(
+            ui.panel(filt["filter_text"],filt["clear_button"]," AND ".join(filt["filter_clauses"]),ui.flex(*filt["filter_buttons"],wrap="wrap"),title="Filtering controls"),
+            ui.panel(ui.flex(*aggs["by_buttons"],wrap="wrap"),aggs["metric_button"],title="Aggregation controls")),
+        ui.row(
+            ui.panel(filt["filtered_table"],title="Filtered table"),
+            ui.panel(aggs["aggregated_table"],title="Aggregated table")
+        ),
+        ui.row(ui.panel(chrt["chart_button"],chrt["chart"],title="Charted aggregated table"),height=60)
     )
--- gui/traces.py
+++ gui/traces.py
@@ -1,4 +1,28 @@
+import typing
+
 import deephaven.plot.express as dx
+
+from deephaven.table import Table
+from deephaven.pandas import to_pandas
 
 import plotly.express as px
 import plotly.graph_objects as go
+
+def bars(t:Table,bys:typing.List[str],metric:str) -> go.Figure:
+
+    df = to_pandas(t)
+
+    fig = go.Figure()
+
+    match len(bys):
+        case 1:
+            fig.add_trace(go.Bar(x=df[bys[0]],y=df[metric],name=bys[0]))
+        case 2:
+            for n in df[bys[1]].unique():
+                fig.add_trace(go.Bar(x=df[df[bys[1]]==n][bys].T,y=df[df[bys[1]]==n][metric],name=str(n)))
+        case _:
+            raise ValueError()
+
+    fig.update_layout(xaxis_title=".".join(bys),yaxis_title=metric)
+
+    return fig
