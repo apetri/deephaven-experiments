cfdef5cbe16f2280e0366595084d763dc4c94c56
Andrea Petri <ap3020@columbia.edu>
2025-08-25 14:52:07 -0400

    add pivot, unpivot table utilities

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import typing
 
-from deephaven import new_table,empty_table
+from deephaven import empty_table,agg,merge
 from deephaven.table import Table
 import deephaven.numpy as dhnp
 
@@ -17,8 +17,42 @@ def hmerge(t1:Table,t2:Table) -> Table:
     t12 = t1.update("__idx = i").natural_join(t2.update("__idx=i"),on="__idx")
     return t12.drop_columns("__idx")
 
-def pivot(t:Table):
-    return empty_table(0)
+def pivot(t:Table,keycols:typing.List[str],namecol:str,valuecol:str) -> Table:
+    """
+    Pivot a table
+    """
 
-def unpivot(t:Table):
-    return empty_table(0)
+    ctyps = {r["Name"]:r["DataType"] for r in t.meta_table.iter_dict()}
+    if not ctyps[namecol]=="java.lang.String":
+        raise ValueError(f"{namecol} column is not a string")
+
+    # Construct the index
+    pvt = t.select_distinct(keycols)
+
+    # Pivoted column names
+    pcols = dhnp.to_numpy(t.select_distinct(namecol))[:,0]
+
+    # Add the columns one at a time via join
+    for pc in pcols:
+        tf = t.where(f"{namecol}=`{pc}`").agg_by(agg.first(f"{pc} = {valuecol}"),by=keycols)
+        pvt = pvt.natural_join(tf,on=keycols)
+
+    # Done
+    return pvt
+
+def unpivot(t:Table,keycols:typing.List[str],keyname:str,valuename:str):
+
+    """
+    Unpivot a table
+    """
+
+    vtyps = [ r["DataType"] for r in t.meta_table.iter_dict() if not r["Name"] in keycols ]
+
+    if not all([v==vtyps[0] for v in vtyps]):
+        raise ValueError("Inconsistent value types")
+
+    chnks = []
+    for cn in [c for c in t.column_names if not c in keycols]:
+        chnks.append(t.select(keycols + [f"{keyname} = `{cn}`",f"{valuename} = {cn}"]))
+
+    return merge(chnks)
